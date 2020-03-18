42988214991582e6c76042e1dd2bcf6c79be08d6
Andrew-PC <Andrew-PC@Andrew>
2020-03-18 16:39:49 +0200

    testing if the code works

--- src/shortestjobfirst/Main.java
+++ src/shortestjobfirst/Main.java
@@ -5,7 +5,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Andrew");
-        System.out.println("Ereny");
+        int x = 5;
+        System.out.println(x);
     }
 }
