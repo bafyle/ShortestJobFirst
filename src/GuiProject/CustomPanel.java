70e3341397d0db7152277e7a50e38d30058a294c
Andrew-PC <Andrew-PC@Andrew>
2020-04-06 20:46:36 +0200

    bug fixed

--- src/GuiProject/CustomPanel.java
+++ src/GuiProject/CustomPanel.java
@@ -11,30 +11,32 @@ public class CustomPanel extends JPanel
     private final int lineY2 = 250;
     private final int StringY = 275;
     private final int numberY = 325;
+    private int y = 150;
     public ArrayList<shortestjobfirst.Process> ps;
     public int biggestNumber;
     @Override
     public void paint(Graphics g)
     {
-        /*g.drawRect(40, 250, 1000, 50);
-        g.drawString(Integer.toString(biggestNumber), 1040, numberY);
+        super.paint(g);
+        g.drawRect(40, 250+y, 1000, 50);
+        //g.drawString(Integer.toString(biggestNumber), 1040, numberY);
         for(shortestjobfirst.Process s : ps)
         {
             for(Integer i : s.startTimes)
             {
                 int x = (int)((double)i / (double)biggestNumber * 1000);
-                g.drawLine(40 + x, lineY1, 40 + x, lineY2);
-                g.drawString(Integer.toString(i), 40 + x, numberY);
-                g.drawString("P"+s.processID, 40+x, StringY);
+                g.drawLine(40 + x, lineY1 + y, 40 + x, lineY2 + y);
+                g.drawString(Integer.toString(i), 40 + x, numberY+y);
+                g.drawString("P"+s.processID, 40+x, StringY+y);
             }
             for(Integer i : s.endTimes)
             {
                 int x = (int)((double)i / (double)biggestNumber * 1000);
-                g.drawLine(40 + x, lineY1, 40 + x, lineY2);
-                g.drawString(Integer.toString(i), 40 + x, numberY);
+                g.drawLine(40 + x, lineY1+y, 40 + x, lineY2+y);
+                g.drawString(Integer.toString(i), 40 + x, numberY+y);
                 //g.drawString("P"+s.processID, 40+x, StringY);
             }
-        }*/
+        }
         
     }
     public CustomPanel(ArrayList<shortestjobfirst.Process> pss, int bn)
